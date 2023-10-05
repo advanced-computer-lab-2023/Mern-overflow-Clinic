953da3030bfbd7602538e878fdcad0f7bd1f9f5b
ahmedyasser07 <ahmedtaweel.65@gmail.com>
2023-10-06 02:08:42 +0300

    update skeleton code

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,8 +15,8 @@ const MongoURI:string = "mongodb+srv://dbuser:987654321@acl.n4q8ykx.mongodb.net/
 const app = express();
 const port:number = parseInt(process.env.PORT!) as number || 8000;
 
-//app.use(express.urlencoded({extended:true}))
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}))
+
 // app.use(express.json);
 
 
@@ -36,6 +36,7 @@ app.get("/PatientPrescriptions",userController.viewPatientPrescription);
 app.get("/SearchPrescriptions",userController.searchPrescriptions)
 
 //POST
+app.post("/AddPatient",userController.addPatient)
 app.post("/AddRequest",userController.addRequest);
 app.post("/AddAdmin",userController.addAdmin);
 app.post("/AcceptRequest",userController.acceptAddDoctorRequest);

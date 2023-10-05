6ba56dd952e9d8bc781bbd49e050257954dc43f2
ahmedyasser07 <ahmedtaweel.65@gmail.com>
2023-10-06 01:56:43 +0300

    routes and methods skeleton

--- server/src/controller/userController.ts
+++ server/src/controller/userController.ts
@@ -6,30 +6,115 @@ import patient from '../models/Patient.ts';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import app from "../index.ts"
 
+export default{
 
-
-function addPatient(req:Request, res:Response){
-    // const newPatient = patient.create(req.body)
-    // .then(newPatient => res.json(newPatient))
-    // .catch((err)=>res.json(err))
-
-    const newPatient = new patient(
-    {
-        username: req.body.username,
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: req.body.passwordHash,
-        dateOfBirth: req.body.dateOfBirth,
-        gender: req.body.gender,
-        mobileNumber: req.body.mobileNumber,
-        emergencyContact: req.body.emergencyContact
+     addPatient(req:Request, res:Response){
+        
+        const newPatient = new patient(
+        {
+            username: req.body.username,
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: req.body.passwordHash,
+            dateOfBirth: req.body.dateOfBirth,
+            gender: req.body.gender,
+            mobileNumber: req.body.mobileNumber,
+            emergencyContact: req.body.emergencyContact
+        }
+        )
+        newPatient.save().then((result)=>{
+            res.send(result);
+        }).catch((err)=>{
+            //console.log(err);
+        });
     }
-    )
-    newPatient.save().then((result)=>{
-        res.send(result);
-    }).catch((err)=>{
-        console.log(err);
-    });
+    ,
+    addRequest(req:Request, res:Response){
+
+    }
+    ,
+    addAdmin(req:Request, res:Response){
+
+    }
+    ,
+    removeDoctor(req:Request, res:Response){
+
+    }
+    ,
+    removePatient(req:Request, res:Response){
+
+    }
+    ,
+    removeAdmin(req:Request, res:Response){
+
+    }
+    ,
+    viewDoctors(req:Request, res:Response){
+
+    }
+    ,
+    acceptAddDoctorRequest(req:Request, res:Response){
+        //add doctor after accepting request
+    }
+    ,
+    rejectAddDoctorRequest(req:Request, res:Response){
+        //reject addition of new doctor to database
+    }
+    ,
+    addPackage(req:Request, res:Response){
+
+    }   
+    ,
+    updatePackage(req:Request, res:Response){
+
+    }
+    ,
+    deletePackage(req:Request, res:Response){
+
+    }
+    ,
+    updateDoctor(req:Request, res:Response){
+
+    }
+    ,
+    addRelative(req:Request, res:Response){
+
+    }
+    ,
+    viewRelatives(req:Request, res:Response){
+
+    }
+    ,
+    viewPatients(req:Request, res:Response){
+
+    }
+    ,
+    viewRecordOfPatients(req:Request, res:Response){
+
+    }
+    ,
+    searchPatient(req:Request, res:Response){
+
+    }
+    ,
+    searchDoctor(req:Request, res:Response){
+
+    }
+    ,
+    viewDoctorDetails(req:Request, res:Response){
+
+    }
+    ,
+    viewPatientPrescription(req:Request, res:Response){
+
+    }
+    ,
+    searchPrescriptions(req:Request, res:Response){
+
+    }
+    
 }
-export default addPatient;
+
+
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,63 +3,58 @@ import mongoose from 'mongoose';
 import axios from 'axios';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import addPatient from './controller/userController.ts'
-import patient from './models/Patient.ts';
+import userController from './controller/userController.ts'
 import exp from "constants";
 import { Request, Response } from 'express';
 
 dotenv.config()
-
 mongoose.set('strictQuery', false);
 
-const MongoURI:string = "mongodb+srv://dbuser:987654321@acl.n4q8ykx.mongodb.net/?retryWrites=true&w=majority"!;
-
-
 //App variables
+const MongoURI:string = "mongodb+srv://dbuser:987654321@acl.n4q8ykx.mongodb.net/?retryWrites=true&w=majority"!;
 const app = express();
 const port:number = parseInt(process.env.PORT!) as number || 8000;
 
-//app.use(express.urlencoded({extended:true}))
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.json);
+
+
 //GET 
 app.get('/', (req,res)=>{
   res.send("hello");
   console.log("hello, world!");
 });
-
-
-
+app.get("/Doctors",userController.viewDoctors);
+app.get("/Relatives",userController.viewRelatives);
+app.get("/Patients",userController.viewPatients);
+app.get("/PatientRecord",userController.viewRecordOfPatients);
+app.get("/SearchPatient",userController.searchPatient);
+app.get("/SearchDoctor",userController.searchDoctor);
+app.get("/DoctorDetails",userController.viewDoctorDetails);
+app.get("/PatientPrescriptions",userController.viewPatientPrescription);
+app.get("/SearchPrescriptions",userController.searchPrescriptions)
 
 //POST
-app.post("/addPatient", (req:Request,res:Response)=>{
-  console.log("");
-  console.log("");
-  console.log("=");
-  console.log(req.body);
-  
-  const newPatient = new patient(
-    {
-        username: req.body.username,
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: req.body.passwordHash,
-        dateOfBirth: req.body.dateOfBirth,
-        gender: req.body.gender,
-        mobileNumber: req.body.mobileNumber,
-        emergencyContact: req.body.emergencyContact
-    }
-    )
-    
-    newPatient.save().then((result)=>{
-        res.send(result);
-    }).catch((err)=>{
-        //console.log(err);
-    });
-});
+app.post("/AddPatient",userController.addPatient);
+app.post("/AddRequest",userController.addRequest);
+app.post("/AddAdmin",userController.addAdmin);
+app.post("/AcceptRequest",userController.acceptAddDoctorRequest);
+app.post("/AddPackage",userController.addPackage);
+app.post("/AddRelative",userController.addRelative);
 
+//PUT
+app.put("/UpdatePackage",userController.updatePackage);
+
+//PATCH
+app.patch("/UpdateDoctorData",userController.updateDoctor);
 
 //DELETE
+app.delete("/RemoveDoctor",userController.removeDoctor);
+app.delete("/RemovePatient",userController.removePatient);
+app.delete("/RemoveAdmin",userController.removeAdmin);
+app.delete("/RejectRequest",userController.rejectAddDoctorRequest);
+app.delete("/DeletePackage",userController.deletePackage);
+
+
 
 
 
@@ -70,3 +65,6 @@ mongoose.connect(MongoURI).then(()=>{
     	console.log(`Listening to requests on http://localhost:${port}`);
   	});
 }).catch(err => console.log(err));
+
+
+export default app;

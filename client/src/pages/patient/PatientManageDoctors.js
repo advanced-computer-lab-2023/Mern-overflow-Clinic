42fcb72b70acb89d7ca314db48d631f54ed7312d
Mohamed Mohey <mmi9433@gmail.com>
2023-10-08 00:38:38 +0300

    added PatientView in PatientManageDoctor

--- client/src/pages/patient/PatientManageDoctors.js
+++ client/src/pages/patient/PatientManageDoctors.js
@@ -1,10 +1,14 @@
 import PatientDashboard from "./PatientDashboard";
+import PatientView from "../../components/ViewComponents/PatientView";
 
 const PatientManageDoctors = () => {
-    return (
-        <PatientDashboard title="View All Doctors" />
-
-     );
-}
+  return (
+    <>
+      <PatientDashboard title="View All Doctors" />
+      <PatientView />
+    </>
+  );
+};
 
 export default PatientManageDoctors;
+

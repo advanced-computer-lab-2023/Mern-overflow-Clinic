6375bfd46c8562d0e8ddec9907de40c4242531fa
Mohamed Mohey <mmi9433@gmail.com>
2023-10-11 17:47:55 +0300

    Fix search in DoctorViewPatients

--- client/src/components/ViewComponents/DoctorViewPatients.js
+++ client/src/components/ViewComponents/DoctorViewPatients.js
@@ -1,72 +1,24 @@
-import {
-  Input,
-  InputLabel,
-  TextField,
-  Grid,
-  Select,
-  MenuItem,
-  Button,
-  Box,
-  Container,
-  FormControl,
-  Typography,
-  Divider,
-  FormLabel,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-} from "@mui/material";
+import { Input, Container, Button, List, ListItem } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Fuse from "fuse.js";
 import axios from "axios";
-
-const columns = [
-  {
-    key: "username",
-    label: "USERNAME",
-  },
-
-  {
-    key: "name",
-    label: "NAME",
-  },
-
-  {
-    key: "email",
-    label: "EMAIL",
-  },
-
-  {
-    key: "dateOfBirth",
-    label: "DATE OF BIRTH",
-  },
-
-  {
-    key: "gender",
-    label: "GENDER",
-  },
-
-  {
-    key: "mobileNumber",
-    label: "MOBILE NUMBER",
-  },
-];
 
 export default function DoctorViewPatients() {
   const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
+  const [Query, setQuery] = useState("");
+  const [selectedPatient, setSelectedPatient] = useState({});
+
+  useEffect(() => {
+    console.log(selectedPatient);
+  }, []);
 
   const fetchTableData = () => {
     axios.get(`http://localhost:8000/patients`).then((res) => {
       setData(res.data);
-      setFilteredData(res.data);
     });
   };
 
@@ -74,60 +26,51 @@ export default function DoctorViewPatients() {
     fetchTableData();
   }, []);
 
-  const searchItem = (query) => {
-    if (!query) {
-      setFilteredData(data);
-      return;
-    }
-    const fuse = new Fuse(filteredData, {
-      keys: ["name"],
-      threshold: 0.3,
-    });
-    const result = fuse.search(query);
-    const finalResult = [];
-    if (result.length) {
-      result.forEach((item) => {
-        finalResult.push(item.item);
-      });
-      setFilteredData(finalResult);
-    } else {
-      setFilteredData([]);
-    }
-  };
-
   return (
     <Container>
       <Container>
         <Input
           size="lg"
-          bordered
-          clearable
           placeholder="Search..."
-          onChange={(e) => searchItem(e.target.value)}
+          onChange={(e) => setQuery(e.target.value)}
         />
       </Container>
       <Table>
         <TableHead>
           <TableRow>
-            {columns.map((column) => (
-              <TableCell key={column.key}>{column.label}</TableCell>
-            ))}
+            <TableCell key="name">Name</TableCell>
+            <TableCell key="email">Email</TableCell>
+            <TableCell key="dateOfBirth">Date of Birth</TableCell>
+            <TableCell key="gender">Gender</TableCell>
+            <TableCell key="mobileNumber">Mobile Number</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {filteredData.map((row) => (
-            <TableRow key={row.username}>
-              <TableCell>{row.username}</TableCell>
-              <TableCell>{row.name}</TableCell>
-
-              <TableCell>{row.email}</TableCell>
-              <TableCell>{row.dateOfBirth}</TableCell>
-              <TableCell>{row.gender}</TableCell>
-              <TableCell>{row.mobileNumber}</TableCell>
-            </TableRow>
-          ))}
+          {data.map(
+            (row) =>
+              row.name.toLowerCase().includes(Query.toLowerCase()) && (
+                <TableRow key={row.username}>
+                  <TableCell>{row.username}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                  <TableCell>{row.dateOfBirth}</TableCell>
+                  <TableCell>{row.gender}</TableCell>
+                  <TableCell>{row.mobileNumber}</TableCell>
+                  <TableCell>
+                    <Button onClick={setSelectedPatient(row)}>
+                      Select Patient
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ),
+          )}
         </TableBody>
       </Table>
+      {selectedPatient !== {} &&
+        selectedPatient.map((value) => (
+          <List>
+            <ListItem>{value}</ListItem>
+          </List>
+        ))}
     </Container>
   );
 }

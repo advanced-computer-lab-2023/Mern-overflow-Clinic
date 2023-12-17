571958c7c806f7d0b71bd84ef3e1493cd9c5b50a
Ahmed Wael <ahmedwael216@protonmail.com>
2023-12-17 03:00:08 +0200

    hot fix

--- client/src/components/ButtonAppBar.js
+++ client/src/components/ButtonAppBar.js
@@ -74,45 +74,44 @@ export default function ButtonAppBar(props) {
 	};
 
 	const handleClose = (link) => {
-		console.log("navigating to link:", link);
-
-		if (link.includes("meet.google")) {
-			window.open(link, "_blank");
-		} else if (link.includes("chat")) {
-			if (!window.location.href.includes("chat")) window.location.href = link;
-		}
-		else {
-			navigate(link);
+		console.log("navigating to link:", typeof link);
+		if (typeof link !== 'object') {
+			if (link.includes("chat")) {
+				if (!window.location.href.includes("chat")) window.location.href = link;
+			}
+			else {
+				navigate(link);
+			}
 		}
 		setAnchorEl(null);
 	};
 
 	const list = (anchor) => (
-        <Box sx={{ display: "flex", justifyContent: "space-between", flexDirection: "column", height: "100%" }}>
-            <Box
-                sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
-                role="presentation"
-                onClick={toggleDrawer(anchor, false)}
-                onKeyDown={toggleDrawer(anchor, false)}
-            >
-                <Box sx={{ my: "30px" }}></Box>
-                <Divider>
-                    <Chip
-                        sx={{ mx: "10px", color: "#333", backgroundColor: "#293241", color: "white", fontSize: "15px" }}
-                        label={`${props.user} Dashboard`}
-                    />
-                </Divider>
-                <Box sx={{ my: "20px" }}></Box>
-                {props.children}
-            </Box>
+		<Box sx={{ display: "flex", justifyContent: "space-between", flexDirection: "column", height: "100%" }}>
+			<Box
+				sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+				role="presentation"
+				onClick={toggleDrawer(anchor, false)}
+				onKeyDown={toggleDrawer(anchor, false)}
+			>
+				<Box sx={{ my: "30px" }}></Box>
+				<Divider>
+					<Chip
+						sx={{ mx: "10px", color: "#333", backgroundColor: "#293241", color: "white", fontSize: "15px" }}
+						label={`${props.user} Dashboard`}
+					/>
+				</Divider>
+				<Box sx={{ my: "20px" }}></Box>
+				{props.children}
+			</Box>
 
-            <Box>
-                <Typography sx={{ fontFamily: "rubik", fontSize: "14px", margin: "10px", color: "#293241" }}>
-                    © 2023 El7a2ny Solutions
-                </Typography>
-            </Box>
-        </Box>
-    );
+			<Box>
+				<Typography sx={{ fontFamily: "rubik", fontSize: "14px", margin: "10px", color: "#293241" }}>
+					© 2023 El7a2ny Solutions
+				</Typography>
+			</Box>
+		</Box>
+	);
 
 	const [state, setState] = React.useState({
 		top: false,
@@ -121,22 +120,22 @@ export default function ButtonAppBar(props) {
 		right: false,
 	});
 
-    const [isVisible, setIsVisible] = useState(false);
+	const [isVisible, setIsVisible] = useState(false);
 
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    };
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth"
+		});
+	};
 
-    const handleScroll = () => {
-        if (window.pageYOffset > 300) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
-    };
+	const handleScroll = () => {
+		if (window.pageYOffset > 300) {
+			setIsVisible(true);
+		} else {
+			setIsVisible(false);
+		}
+	};
 
 	const toggleDrawer = (anchor, open) => (event) => {
 		if (
@@ -164,47 +163,47 @@ export default function ButtonAppBar(props) {
 	};
 
 	return (
-        <>
-            <Zoom in={isVisible}>
-                <Fab color="primary" onClick={scrollToTop} sx={{ position: "fixed", bottom: 30, right: 30 }}>
-                    <ArrowUpwardIcon />
-                </Fab>
-            </Zoom>
-            <div>
-                <Drawer anchor={"left"} open={state["left"]} onClose={toggleDrawer("left", false)}>
-                    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                        <img src={logo} alt="Logo" style={{ width: "50px", height: "auto", margin: "10px" }} />
-                        <IconButton
-                            variant="contained"
-                            sx={{ height: "50px", width: "50px", margin: "10px" }}
-                            onClick={toggleDrawer("left", false)}
-                        >
-                            <CloseIcon />
-                        </IconButton>
-                    </Box>
-                    <Typography sx={{ mx: "10px", fontFamily: "cursive", fontWeight: "bold", color: "#1564C0" }}>
-                        {" "}
-                        El7a2ny Clinic
-                    </Typography>
-                    {list("left")}
-                </Drawer>
-            </div>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                            onClick={toggleDrawer("left", true)}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: "left" }}>
-                            {props.title}
-                        </Typography>
+		<>
+			<Zoom in={isVisible}>
+				<Fab color="primary" onClick={scrollToTop} sx={{ position: "fixed", bottom: 30, right: 30 }}>
+					<ArrowUpwardIcon />
+				</Fab>
+			</Zoom>
+			<div>
+				<Drawer anchor={"left"} open={state["left"]} onClose={toggleDrawer("left", false)}>
+					<Box sx={{ display: "flex", justifyContent: "space-between" }}>
+						<img src={logo} alt="Logo" style={{ width: "50px", height: "auto", margin: "10px" }} />
+						<IconButton
+							variant="contained"
+							sx={{ height: "50px", width: "50px", margin: "10px" }}
+							onClick={toggleDrawer("left", false)}
+						>
+							<CloseIcon />
+						</IconButton>
+					</Box>
+					<Typography sx={{ mx: "10px", fontFamily: "cursive", fontWeight: "bold", color: "#1564C0" }}>
+						{" "}
+						El7a2ny Clinic
+					</Typography>
+					{list("left")}
+				</Drawer>
+			</div>
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton
+							size="large"
+							edge="start"
+							color="inherit"
+							aria-label="menu"
+							sx={{ mr: 2 }}
+							onClick={toggleDrawer("left", true)}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: "left" }}>
+							{props.title}
+						</Typography>
 						<div>
 							<IconButton onClick={handleClick} color="inherit">
 								{newNotifications ? (
@@ -225,20 +224,20 @@ export default function ButtonAppBar(props) {
 								))}
 							</Menu>
 						</div>
-                        <IconButton
-                            size="large"
-                            edge="end"
-                            color="inherit"
-                            aria-label="logout"
-                            sx={{ mr: 2 }}
-                            component={Link}
-                            onClick={handleLogout}
-                        >
-                            <LogoutIcon />
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-        </>
-    );
+						<IconButton
+							size="large"
+							edge="end"
+							color="inherit"
+							aria-label="logout"
+							sx={{ mr: 2 }}
+							component={Link}
+							onClick={handleLogout}
+						>
+							<LogoutIcon />
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+			</Box>
+		</>
+	);
 }
